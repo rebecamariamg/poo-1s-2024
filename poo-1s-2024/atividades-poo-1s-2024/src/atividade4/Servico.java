c54d2a7e56be554d91e9032802ddc6d7acfb3c4e
rebeca <rebecamariamg@gmail.com>
2024-04-27 00:06:56 -0300

    aatv 4

---
+++ poo-1s-2024/atividades-poo-1s-2024/src/atividade4/Servico.java
@@ -0,0 +1,20 @@
+package atividade4;
+
+public class Servico {
+    private String nome;
+    private double preco;
+
+    public Servico(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
+
